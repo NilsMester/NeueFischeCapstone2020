615f9647cd4bb96bea88d41b14fea9906897be53
Nils Mester <n-mester@mailbox.org>
2020-11-18 14:19:29 +0100

    B - rewrite addRecord with query

--- backend/src/test/java/de/neuefische/capstoneproject/link_librarian/service/RecordServiceTest.java
+++ backend/src/test/java/de/neuefische/capstoneproject/link_librarian/service/RecordServiceTest.java
@@ -8,6 +8,9 @@ import de.neuefische.capstoneproject.link_librarian.utilities.IdUtilities;
 import de.neuefische.capstoneproject.link_librarian.utilities.TimeStampUtilities;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.time.Instant;
 import java.util.*;
@@ -89,5 +92,85 @@ void addRecordTest(){
         assertThat(addedRecord,is(expectedRecord));
         verify(linkLibrarianUserDao).save(expectedUser);
 }
+
+        @Test
+        @DisplayName("The \"add\" method should return status FORBIDDEN")
+        void addRecordForbiddenTest() {
+                //Given
+                String principalName = "saskia@web.de";
+                String expectedId = "uniqueId";
+                Instant expectedTime = Instant.parse("2020-10-26T10:00:00Z");
+
+                LinkLibrarianUser userAlex = new LinkLibrarianUser(
+                        "alex@web.de",
+                        new ArrayList<>(List.of(
+                                new Record("1",
+                                        "https://dev.to/medhatdawoud/gradient-borders-with-curves-and-3d-movement-in-css-nextjs-ticket-clone-3cho",
+                                        "tutorial box gradient borders with curves ",
+                                        expectedTime,
+                                        true,
+                                        new ArrayList<>(List.of("Css", "Styled-component")))))
+                );
+
+                AddRecordDto recordToAdd = new AddRecordDto(
+                        "https://react.semantic-ui.com/modules/sidebar/#examples-transitions",
+                        "nice sidebar",
+                        true,
+                        new ArrayList<>(List.of("React", "Css", "Styled-component"))
+                );
+
+                //When
+                when(idUtilities.generateId()).thenReturn(expectedId);
+                when(timeStampUtilities.generateTimestampEpochSeconds()).thenReturn(expectedTime);
+                when(linkLibrarianUserDao.findById(principalName)).thenReturn(Optional.of(userAlex));
+
+                //Then
+                try {
+                        recordService.addRecord(recordToAdd, principalName);
+                        fail("missing exception");
+                }catch (ResponseStatusException exception) {
+                        assertThat(exception.getStatus(), is(HttpStatus.FORBIDDEN));
+                }
+        }
+
+        @Test
+        @DisplayName("The \"add\" method should return status FORBIDDEN")
+        void addRecordUserNotFoundTest(){
+                //Given
+                String principalName = "alex@web.de";
+                String expectedId = "uniqueId";
+                Instant expectedTime = Instant.parse("2020-10-26T10:00:00Z");
+
+                LinkLibrarianUser userAlex = new LinkLibrarianUser(
+                        "alex@web.de",
+                        new ArrayList<>(List.of(
+                                new Record("1",
+                                        "https://dev.to/medhatdawoud/gradient-borders-with-curves-and-3d-movement-in-css-nextjs-ticket-clone-3cho",
+                                        "tutorial box gradient borders with curves ",
+                                        expectedTime,
+                                        true,
+                                        new ArrayList<>(List.of("Css", "Styled-component")))))
+                );
+
+                AddRecordDto recordToAdd = new AddRecordDto(
+                        "https://react.semantic-ui.com/modules/sidebar/#examples-transitions",
+                        "nice sidebar",
+                        true,
+                        new ArrayList<>(List.of("React", "Css", "Styled-component"))
+                );
+
+                //When
+                when(idUtilities.generateId()).thenReturn(expectedId);
+                when(timeStampUtilities.generateTimestampEpochSeconds()).thenReturn(expectedTime);
+                when(linkLibrarianUserDao.findById(principalName)).thenReturn(Optional.empty());
+
+                //Then
+                try {
+                        recordService.addRecord(recordToAdd, principalName);
+                        fail("missing exception");
+                }catch (ResponseStatusException exception) {
+                        assertThat(exception.getStatus(), is(HttpStatus.NOT_FOUND));
+                }
+        }
 
 }
