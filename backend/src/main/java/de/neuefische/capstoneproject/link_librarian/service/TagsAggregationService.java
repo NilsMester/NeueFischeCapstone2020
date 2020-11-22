4953d80448a20f4894b04ac60d572518444c4206
Nils Mester <n-mester@mailbox.org>
2020-11-22 09:04:54 +0100

    Test 2 Tags Aggregation

--- backend/src/main/java/de/neuefische/capstoneproject/link_librarian/service/TagsAggregationService.java
+++ backend/src/main/java/de/neuefische/capstoneproject/link_librarian/service/TagsAggregationService.java
@@ -34,43 +34,6 @@ public class TagsAggregationService {
         this.mongoTemplate = mongoTemplate;
     }
 
- /*  public List<String> tagList (String principalName){
-        Query query = new Query();
-        query.addCriteria(Criteria.where("email").is(principalName));
-        query.fields().include("tagsList");
-
-        List<String> list = mongoTemplate.find(query, String.class);
-
-        Set<Object> tagSet = new HashSet<Object>();
-
-        *//*for (Object tag : list)
-            if(!tagSet.contains(tag))
-                tagSet.add(tag);
-            else(
-*//*
-        return list;
-    }*/
-
-    /*Arrays.asList(
-            Aggregates.match(Filters.eq("email", principalName)),
-                        *//*group("$", Accumulators.sum("count", 1)
-                        Aggregates.unwind()*//*
-            Aggregates.project(
-            Projections.fields(
-            Projections.exclude(),
-            Projections.include("tagsList")
-            )
-            )
-            )*/
-
-  /*  db.linklibrarianuser.aggregate([{"$match":{"_id":"saskia@web.net"}},
-    {"$unwind":"$recordList"},
-
-    {$group:{_id:"$recordList.tagsList", count:{$sum:1}}},
-    {"$unwind":"$_id"},
-
-            ])*/
-
   /*  db.linklibrarianuser.aggregate([{"$match":{"_id":"saskia@web.net"}},
     {$group:{_id:"$recordList.tagsList"}},
     {$project:{_id:1, tagsList:1}},
@@ -115,4 +78,20 @@ public class TagsAggregationService {
     }*/
 
 
+/*  public List<String> tagList (String principalName){
+        Query query = new Query();
+        query.addCriteria(Criteria.where("email").is(principalName));
+        query.fields().include("tagsList");
+
+        List<String> list = mongoTemplate.find(query, String.class);
+
+        Set<Object> tagSet = new HashSet<Object>();
+
+        *//*for (Object tag : list)
+            if(!tagSet.contains(tag))
+                tagSet.add(tag);
+            else(
+*//*
+        return list;
+    }*/
 }
