bf48af39ec2b7abc8963a2299736abd91dac6ea9
Nils Mester <n-mester@mailbox.org>
2020-11-25 09:50:07 +0100

    usereducer

--- frontend/src/commons/RecordForm.js
+++ frontend/src/commons/RecordForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
 import {useHistory} from 'react-router-dom';
 import styled from 'styled-components/macro';
 import UserTagList from "./UserTagList";
@@ -6,16 +6,24 @@ import SideBarActionButton from "./SideBarActionButton";
 import RecordTagsList from "./RecordTagsList";
 
 const initialState = {
+    titel:"",
     recordLink: '',
     tagsList: [],
     description: '',
     publicStatus: true,
 }
 
-export default function RecordForm({onSave, record = initialState}) {
-    const [recordData, setRecordData] = useState(record);
-    const [recordTags, setRecordTags] = useState(record.tagsList);
+function reducer(state, {field,value}){
+    return {...state, [field]:value}
+}
+
+export default function RecordForm({onSave,onChange, onAddTagKlick, recordOnChange = initialState}) {
+    const [recordTagsListInProgress, setRecordTagsListInProgress] = useState(recordOnChange.tagsList)
     const history = useHistory();
+
+    const[state, dispatch] = useReducer(reducer, recordOnChange)
+
+    const {titel, recordLink, tagsList, description} = state
 
     return (
         <FormStyled onSubmit={handleSubmit}>
@@ -23,7 +31,7 @@ export default function RecordForm({onSave, record = initialState}) {
                 <label>
                     Titel
                     <input name="titel"
-                           value={recordData.titel || ""}
+                           value={titel || ""}
                            onChange={handleChange}
                            type="text"/>
                 </label>
@@ -31,17 +39,17 @@ export default function RecordForm({onSave, record = initialState}) {
                 <label>
                     RecordLink
                     <input name="recordLink"
-                           value={recordData.recordLink || ""}
+                           value={recordLink || ""}
                            onChange={handleChange}
                            type="text"/>
                 </label>
                 <p>Tags</p>
-                <RecordTagsList tagsList={recordData.tagsList}/>
+                <RecordTagsList tagsList={tagsList}/>
 
                 <label>
                     Description
                     <input name="description"
-                           value={recordData.description || ""}
+                           value={description || ""}
                            onChange={handleChange}
                            type="test"/>
                 </label>
@@ -49,45 +57,54 @@ export default function RecordForm({onSave, record = initialState}) {
                 <button type="button" onClick={onCancel}>Cancel</button>
                 <button>Save</button>
             </DivStyled>
+
             <SidebarStyled>
+
                 <SidebarSection1Styled/>
+
                 <SidebarSection2Styled>
                     <SideBarActionButton>Tags</SideBarActionButton>
                 </SidebarSection2Styled>
+
                 <SidebarSection3Styled>
                     <UserTagList/>
                 </SidebarSection3Styled>
+
                 <SidebarSection2Styled>
                     <SideBarActionButton>New Tag</SideBarActionButton>
                 </SidebarSection2Styled>
+
                 <SidebarSection4Styled>
                     <input name="newTag"
-                           value={recordTags || ""}
-                           onChange={event => setRecordTags(event.target.value)}
+                           value={recordTagsListInProgress || ""}
+                           onChange={event => setRecordTagsListInProgress([event.target.value])}
                            type="text"/>
-                    <button type="button" onClick={handleTagKlick}>Add Tag</button>
+                    <button type="button" onClick={handleTagLickButton}>Add Tag</button>
                 </SidebarSection4Styled>
+
             </SidebarStyled>
 
         </FormStyled>
     );
 
     function handleChange(event) {
-        setRecordData({...recordData, [event.target.name]: event.target.value});
+        dispatch({field: event.target.name, value: event.target.value});
+        onChange(state)
     }
 
     function onCancel() {
         history.goBack();
     }
 
-    function handleTagKlick() {
-        setRecordData({...recordData, tagsList: [...recordData.tagsList, recordTags]});
-        setRecordTags("");
+    function handleTagLickButton() {
+        dispatch({...state, tagsList: [...state.tagsList, recordTagsListInProgress]});
+        onAddTagKlick(state);
+        setRecordTagsListInProgress("");
     }
 
     function handleSubmit(event) {
         event.preventDefault();
-        onSave(recordData);
+        onSave(state);
     }
 }
 
@@ -104,7 +121,7 @@ grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
 const SidebarStyled = styled.div`
 display: grid;
 align-content: end;
-justify-content: end;
+justify-content: end;<
 row-gap: 12px;
 `
 const SidebarSection1Styled = styled.section`
--- frontend/src/pages/addRecordPage/AddRecordPage.js
+++ frontend/src/pages/addRecordPage/AddRecordPage.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import RecordForm from "../../commons/RecordForm";
 import RecordContext from "../../contexts/RecordContext";
 import { useHistory } from 'react-router-dom';
@@ -6,20 +6,31 @@ import Header from "../../commons/Header";
 
 export default function AddRecordPage() {
     const{createRecord} = useContext(RecordContext)
+
+    const [recordData, setRecordData] = useState();
+    const [recordTags, setRecordTags] = useState();
+
     const history = useHistory();
 
     return(
         <>
-        <Header titel="New Record"/>
-        <RecordForm onSave={handleSave}/>
+            <Header titel="New Record"/>
+            <RecordForm recordOnChange={recordData} onSave={handleSave} onChange={handleChange} onAddTagKlick={handleTagKlick}/>
         </>
     )
 
     function handleSave(record) {
-        const{titel, recordLink, description, publicStaus, tagsList} = record;
-        createRecord(titel, recordLink, description, publicStaus, tagsList);
+        const{titel, recordLink, description, publicStatus, tagsList} = record;
+        createRecord(titel, recordLink, description, publicStatus, tagsList);
         history.push('/');
+    }
 
+    function handleChange(record) {
+        setRecordData(record);
+    }
+
+    function handleTagKlick(record) {
+        setRecordData(record);
     }
 
 }
