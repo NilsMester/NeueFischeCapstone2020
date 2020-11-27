dd9146bd49982dbaa2a1012af3b81138356fea63
Nils Mester <n-mester@mailbox.org>
2020-11-27 16:34:21 +0100

    restructure

--- frontend/src/components/services/TagsToggler.js
+++ frontend/src/components/services/TagsToggler.js
@@ -1,12 +1,2 @@
-import {useContext} from "react";
-import TagsContext from "../../contexts/TagsContext";
 
 
-const {userTagsList, setUserTagsList} = useContext(TagsContext)
-
-const addUserTagToRecordTagsList = (recordDataTagsList) => {
-
-
-}
-
-export default addUserTagToRecordTagsList
