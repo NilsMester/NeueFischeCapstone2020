6db5e11f29feca2048ad89eeb2b769164d4a7c89
Nils Mester <n-mester@mailbox.org>
2020-12-12 20:28:26 +0100

    small changes

--- frontend/src/components/tags/getRecentTags.js
+++ frontend/src/components/tags/getRecentTags.js
@@ -2,13 +2,13 @@
 export default function getRecentTags(userTagList) {
         if (userTagList.length > 5) {
             return(userTagList
-                /*.sort((tagA, tagB) => {
+                .sort((tagA, tagB) => {
                     if (tagA.count > tagB.count) {
                         return -1
                     } else {
                         return 1
                     }
-                })*/
+                })
                 .map(tagItem => tagItem._id)
                 .slice(-userTagList.length, 5))
         } else {
